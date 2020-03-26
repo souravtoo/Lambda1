5f6e445b055f62a0d3e1c4bedb73e1b4358e88e9
Michael Wunderlich <mwunderl@amazon.com>
2020-03-26 13:10:35 -0700

    handle errors

--- sample-apps/blank-go/function/main.go
+++ sample-apps/blank-go/function/main.go
@@ -8,7 +8,7 @@ import (
   "context"
   "encoding/json"
   runtime "github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-lambda-go/events"
+  "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambdacontext"
   "github.com/aws/aws-sdk-go/aws/session"
   "github.com/aws/aws-sdk-go/service/lambda"
@@ -16,28 +16,25 @@ import (
 
 var client = lambda.New(session.New())
 
-func callLambda(){
+func callLambda() (string, error) {
   // Example sending a request using the GetAccountSettingsRequest method.
   input := &lambda.GetAccountSettingsInput{}
   req, resp := client.GetAccountSettingsRequest(input)
-
   err := req.Send()
-  if err == nil {
-      log.Println(resp)
-  }
+  output, _ := json.Marshal(resp.AccountUsage)
+  return string(output), err
 }
 func handleRequest(ctx context.Context, event events.SQSEvent) (string, error) {
-	// event
-	eventJson, _ := json.MarshalIndent(event, "", "  ")
-	log.Printf("EVENT: %s", eventJson)
-	// environment variables
+  // event
+  eventJson, _ := json.MarshalIndent(event, "", "  ")
+  log.Printf("EVENT: %s", eventJson)
+  // environment variables
   log.Printf("REGION: %s", os.Getenv("AWS_REGION"))
   log.Println("ALL ENV VARS:")
   for _, e := range os.Environ() {
     pair := strings.SplitN(e, "=", 2)
     fmt.Println(pair[0])
   }
-  
   // request context
   lc, _ := lambdacontext.FromContext(ctx)
   log.Printf("REQUEST ID: %s", lc.AwsRequestID)
@@ -47,8 +44,11 @@ func handleRequest(ctx context.Context, event events.SQSEvent) (string, error) {
   deadline, _ := ctx.Deadline()
   log.Printf("DEADLINE: %s", deadline)
   // AWS SDK call
-  callLambda()
-  return fmt.Sprint("200 OK"), nil
+  usage, err := callLambda()
+  if err != nil {
+    return "ERROR", err
+  }
+  return fmt.Sprint(usage), nil
 }
 
 func main() {
